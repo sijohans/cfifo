28d8881e4917fbcfef21b7cea06f5bbfdae893ed
Simon Johansson <sijohans@kth.se>
2018-02-05 23:00:17 +0100

    Test adding code coverage

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 mkdir -p build
 cd build
-#-DSANITIZE_ADDRESS=On -DSANITIZE_UNDEFINED=On
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DSANITIZE_ADDRESS=On -DSANITIZE_UNDEFINED=On ..
 make
 make test
-#cppcheck --project=compile_commands.json --enable=all --suppress=missingIncludeSystem --std=c89
+
 lcov --base-directory .. --directory .. --capture --output-file coverage.info
 genhtml -o . coverage.info
